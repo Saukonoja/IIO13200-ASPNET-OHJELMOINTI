0889e0bfa81639fb0f1eaa6b6aeb6ee504098645
Timo Saukonoja <H3694@Labranet.jamk.fi>
2016-09-08 18:34:42 +0300

    First two sites

---
+++ Saitti/HaeTyontekijatIlta.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Configuration; //webconfigia varten
+using System.Data; //ADO.NETin luokkia varten
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class HaeTyontekijatIlta : System.Web.UI.Page
+{
+    string xmlfile;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //luetaan web.configista xml-tiedoston nimi
+        xmlfile = ConfigurationManager.AppSettings["tiedosto"];
+        lblMessage.Text = xmlfile;
+    }
+
+    protected void btnGet_Click(object sender, EventArgs e)
+    {
+        //luetaan xml-tiedostosta työntekijöitten tiedot ja esitetään ne GridViewssa
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        DataView dv = new DataView();
+
+        ds.ReadXml(Server.MapPath(xmlfile));
+        dt = ds.Tables[0];
+        dv = dt.DefaultView;
+
+        //datan sitominen ui controlliin
+        gvData.DataSource = dv;
+        gvData.DataBind();
+        lblMessage.Text = string.Format("Found {0} workers", dt.Rows.Count);
+    }
+}
