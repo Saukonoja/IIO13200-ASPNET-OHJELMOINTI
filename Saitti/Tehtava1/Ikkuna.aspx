691267f28aabeb2c22f248df1152c08645f032c4
Saukonoja <timo.saukonoja@gmail.com>
2016-11-26 20:49:54 +0200

    Finish Tehtava1

---
+++ Saitti/Tehtava1/Ikkuna.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Ikkuna.aspx.cs" Inherits="Tehtava1_Ikkuna" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblWinWidth" runat="server" Text="Ikkunan leveys (mm):"></asp:Label>
+            <asp:TextBox ID="txtWinWidth" runat="server"></asp:TextBox><br />
+            <br />
+            <asp:Label ID="lblWinHeight" runat="server" Text="Ikkunan korkeus (mm):"></asp:Label>
+            <asp:TextBox ID="txtWinHeight" runat="server"></asp:TextBox><br />
+            <br />
+            <asp:Label ID="lblFrWidth" runat="server" Text="Karmipuun leveys (mm):"></asp:Label>
+            <asp:TextBox ID="txtFrWidth" Text="45" runat="server"></asp:TextBox><br />
+            <br />
+            <asp:Button ID="btnCalculate" runat="server" Text="Laske tarjoushinta" OnClick="btnCalculate_Click" /><br />
+            <br />
+            <asp:Label ID="lblWinArea" runat="server" Text="Ikkunan pinta-ala:"></asp:Label>
+            <asp:Label ID="lblAreaResult" runat="server" Text=""></asp:Label><br />
+            <br />
+            <asp:Label ID="lblFrGirth" runat="server" Text="Karmin piiri:"></asp:Label>
+            <asp:Label ID="lblGirthResult" runat="server" Text=""></asp:Label><br />
+            <br />
+            <asp:Label ID="lblPrice" runat="server" Text="Tarjoushinta (ilman ALV):"></asp:Label>
+            <asp:Label ID="lblPriceResult" runat="server" Text=""></asp:Label><br />
+            <br />
+            <asp:Label ID="lblMessage" runat="server" Text="" Font-Bold="True"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/Tehtava1/Ikkuna.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+public partial class Tehtava1_Ikkuna : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+
+    }
+
+    protected void btnCalculate_Click(object sender, EventArgs e) {
+        double WinWidth;
+        double WinHeight;
+        double FrWidth;
+        double Margin;
+        double WinSqrPrice;
+        double AlFramePrice;
+        double Workcost;
+        double Area;
+        double Girth;
+        double Price;
+        
+        try {
+            WinWidth = double.Parse(txtWinWidth.Text);
+            WinHeight = double.Parse(txtWinHeight.Text);
+            FrWidth = double.Parse(txtFrWidth.Text);
+            Margin = double.Parse(ConfigurationManager.AppSettings["Margin"]);
+            WinSqrPrice = double.Parse(ConfigurationManager.AppSettings["WinSqrPrice"]);
+            AlFramePrice = double.Parse(ConfigurationManager.AppSettings["AlFramePrice"]);
+            Workcost = double.Parse(ConfigurationManager.AppSettings["Workcost"]);
+
+            Area = WinWidth * WinHeight / 1000000;
+            lblAreaResult.Text = Area.ToString("0.##") + " m^2";
+            Girth = (((WinWidth * 2) + (WinHeight * 2) + FrWidth * 4) / 1000);
+            lblGirthResult.Text = Girth.ToString("0.##") + " m";
+            Price = (1 + (Margin / 100)) * ((Area * WinSqrPrice) + (Girth * AlFramePrice) + (Workcost));
+            lblPriceResult.Text = Price.ToString("0.##") + " €";
+            lblMessage.Text = "Ok";
+
+        } catch (Exception) {
+
+            lblMessage.Text = "Jokin meni väärin. Täytitkö kaikki kentät?";
+        }
+    }
+
+    
+}
