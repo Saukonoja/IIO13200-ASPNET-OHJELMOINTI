514b76d69874774aede398c2d10eef0646a2f8bf
Saukonoja <timo.saukonoja@gmail.com>
2016-11-27 12:16:18 +0200

    Finish Tehtava3

---
+++ Saitti/Tehtava3/Calendar.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Calendar.aspx.cs" Inherits="Tehtava3_Calendar" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Tänään on: <asp:Label ID="lblToday" runat="server"></asp:Label><br />
+        Valittu päivä: <asp:Label ID="lblSelectedDay" runat="server"></asp:Label><br />
+        Valitun päivän ja tämän päivän välillä on: <asp:Label ID="lblDifference" runat="server"></asp:Label><br />
+        <asp:Button ID="btnPreviousYear" runat="server" Text="<< vuosi" OnClick="btnPreviousYear_Click" />
+        <asp:Button ID="btnNextYear" runat="server" Text="vuosi >>" OnClick="btnNextYear_Click" /><br />
+        <asp:Calendar ID="calendar" runat="server" OnSelectionChanged="calendar_SelectionChanged" AutoPostBack="True"></asp:Calendar> 
+    </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/Tehtava3/Calendar.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Tehtava3_Calendar : System.Web.UI.Page {
+    DateTime today;
+    protected void Page_Load(object sender, EventArgs e) {
+        today = DateTime.Today;      
+        lblToday.Text = today.ToString("dd.MM.yyyy");
+        if (!IsPostBack) {
+            calendar.VisibleDate = today;
+        }
+    }
+
+    protected void btnNextYear_Click(object sender, EventArgs e) {
+        calendar.VisibleDate = calendar.VisibleDate.AddYears(1);
+    }
+
+    protected void btnPreviousYear_Click(object sender, EventArgs e) {
+        calendar.VisibleDate = calendar.VisibleDate.AddYears(-1);
+    }
+
+    protected void calendar_SelectionChanged(object sender, EventArgs e) {
+        DateTime selectedDate = calendar.SelectedDate;
+        string selectedDateStr = selectedDate.ToString("dd.MM.yyyy");
+        selectedDateStr += " vuosi: ";
+        selectedDateStr += calendar.SelectedDate.Year;
+        lblSelectedDay.Text = selectedDateStr;
+        if (selectedDate <= today) {
+            DateTime zerotime = new DateTime(1, 1, 1);
+            TimeSpan timespan = today - selectedDate;
+            int years = (zerotime + timespan).Year - 1;
+            int months = (zerotime + timespan).Month - 1;
+            int days = (zerotime + timespan).Day - 1;
+            lblDifference.Text = years + " vuotta " + months + " kuukautta ja " + days + " päivää.";
+        } else {
+            lblDifference.Text = "Valitsemasi päivä on tulevaisuudessa";
+        }
+    }
+}
