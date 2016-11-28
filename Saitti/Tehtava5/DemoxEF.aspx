d83885a76b583902320e9ec3d091eafcc8805e21
Saukonoja <timo.saukonoja@gmail.com>
2016-11-28 19:56:35 +0200

    Finish Tehtava5

---
+++ Saitti/Tehtava5/DemoxEF.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="DemoxEF.aspx.cs" Inherits="Tehtava5_DemoxEF" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+    <div style="float:left;">
+        <asp:Button ID="btnGetAllCustomers" Text="Hae kaikki asiakkaat" OnClick="btnGetAllCustomers_Click" runat="server" />
+        <asp:DropDownList ID="ddlCountries" AutoPostBack="true" runat="server"></asp:DropDownList>
+        <asp:Button ID="btnGetFromSelected" Text="Hae valitusta maasta" OnClick="btnGetFromSelected_Click" runat="server" />
+        <asp:Button ID="btnGetByCountry" Text="Hae maittain" OnClick="btnGetByCountry_Click" runat="server" />
+        <asp:GridView ID="gvCustomers" runat="server" />
+        <asp:Label ID="lblMessages" runat="server" />
+    </div>
+    <div style="float:left;">
+        <asp:Label ID="lblByCountry" runat="server"></asp:Label>
+    </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="footer" Runat="Server">
+</asp:Content>
+
---
+++ Saitti/Tehtava5/DemoxEF.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Tehtava5_DemoxEF : System.Web.UI.Page {
+    protected static DemoxOyEntities2 ctx;
+    protected void Page_Load(object sender, EventArgs e) {
+        if (!IsPostBack) {
+            ctx = new DemoxOyEntities2();
+            IniAll();
+        }
+        
+    }
+    protected void IniAll() {
+        var country = ctx.asiakas.Select(x => new { maa = x.maa }).OrderBy(x => x.maa).Distinct();
+        ddlCountries.DataSource = country.ToList();
+        ddlCountries.DataTextField = "maa";
+        ddlCountries.DataBind();
+    }
+
+    protected void btnGetAllCustomers_Click(object sender, EventArgs e) {
+        try {
+            gvCustomers.DataSource = ctx.asiakas.ToList();
+            gvCustomers.DataBind();
+            int i = ctx.asiakas.Count();
+            lblMessages.Text = string.Format("Haettu {0} asiakkaan tiedot", i);
+        } catch (Exception ex) {
+
+            lblMessages.Text = ex.Message;
+        }
+        
+    }
+
+
+    protected void btnGetFromSelected_Click(object sender, EventArgs e) {
+        try {
+            string country = ddlCountries.Text;
+            var customer = from c in ctx.asiakas where c.maa == country select c;
+            gvCustomers.DataSource = customer.ToList();
+            gvCustomers.DataBind();
+            int i = customer.Count();
+            lblMessages.Text = string.Format("Haettu {0} asiakkaan tiedot", i);
+        } catch (Exception ex) {
+
+            lblMessages.Text = ex.Message;
+        }
+        
+    }
+
+    protected void btnGetByCountry_Click(object sender, EventArgs e) {
+        try {
+            lblByCountry.Text = "";
+            var country = from x in ctx.asiakas select x.maa;
+            List<string> countryList = country.Distinct().ToList();
+            List<string> customersList = new List<string>();
+
+            foreach (var country1 in countryList) {
+                var customer = from x in ctx.asiakas where x.maa == country1 select x.asnimi;
+                customersList = customer.ToList();
+
+                foreach (var customer1 in customersList) {
+                    var labelText = lblByCountry.Text;
+
+                    if (!labelText.Contains(country1)) {
+                        lblByCountry.Text += "<br><span style='color: green'>" + country1 +
+                            "</span><br><br><span style = 'color: blue';>" + customer1.ToString() + "</span><br>";
+                    } else {
+                        lblByCountry.Text += "<span style='color: blue';>" + customer1.ToString() + "</span><br>";
+                    }
+                }
+            }
+
+            } catch (Exception ex) {
+
+            lblMessages.Text = ex.Message;
+        }
+    }
+}
