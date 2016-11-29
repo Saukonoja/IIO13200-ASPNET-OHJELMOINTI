b0f0e8f5e906d7d22c57bdc9917dd8a810673599
Saukonoja <timo.saukonoja@gmail.com>
2016-11-30 01:14:55 +0200

    Finish Tehtava8

---
+++ Saitti/Tehtava8/Finnkino.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Finnkino.aspx.cs" Inherits="Tehtava8_Finnkino" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div >
+        <div style="float:left;">
+            <asp:ListBox ID="lbTheaters" runat="server" AutoPostBack="true" OnSelectedIndexChanged="lbTheaters_SelectedIndexChanged" Height="480"></asp:ListBox><br />
+        
+        </div>       
+        
+        <div id="movies" runat="server" style="width:1200px; max-height:370px; float:left;">
+            
+
+        </div>
+        
+    </div>
+    </form>
+    <footer>
+        <asp:Label ID="lblMessages" runat="server" Width="100%"></asp:Label>
+    </footer>
+</body>
+    
+</html>
---
+++ Saitti/Tehtava8/Finnkino.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Web.UI.HtmlControls;
+
+
+public partial class Tehtava8_Finnkino : System.Web.UI.Page {
+    XmlDocument xml = new XmlDocument();
+    protected void Page_Load(object sender, EventArgs e) {
+        if (!IsPostBack) {
+            try {
+                xml.Load("http://www.finnkino.fi/xml/TheatreAreas/");
+                XmlNodeList nodes = xml.SelectNodes("/TheatreAreas/TheatreArea");
+                foreach (XmlNode node in nodes) {
+                    lbTheaters.Items.Add(new ListItem(node["Name"].InnerText, node["ID"].InnerText));
+                }
+            } catch (Exception ex) {
+
+                lblMessages.Text = ex.Message;
+            }  
+        }
+    }
+
+
+    protected void lbTheaters_SelectedIndexChanged(object sender, EventArgs e) {
+        lblMessages.Text = "Elokuvateatteri " + lbTheaters.SelectedItem.Text + " valittu";
+        GetImages();
+
+    }
+
+    protected void GetImages() {
+        string areaID = lbTheaters.SelectedValue;
+        xml.Load("http://www.finnkino.fi/xml/Schedule/?area=" + areaID + "&dt=" + DateTime.Today.ToString("dd.MM.yyyy"));
+        XmlNodeList nodes = xml.SelectNodes("/Schedule/Shows/Show/Images/EventLargeImagePortrait");
+        foreach (XmlNode node in nodes) {
+            HtmlGenericControl movieDiv = (HtmlGenericControl)FindControl("movies");
+            HtmlGenericControl img = new HtmlGenericControl("img");
+            img.Attributes["src"] = node.InnerText;
+            movieDiv.Controls.Add(img);
+        }
+    }
+}
