5d23c21bde1fe6b299af5933379ac0a876309eeb
Saukonoja <timo.saukonoja@gmail.com>
2016-11-29 19:52:28 +0200

    Finish Tehtava6

---
+++ Saitti/Tehtava6/Albumi.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Albumi.aspx.cs" Inherits="Tehtava6_Albumi" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+    <img src="" id="albumImage" runat="server"/>
+    <div id="albumInfo" style="font-size:20px;font-weight:bold" runat="server"></div>
+    <div style="font-size:20px;font-weight:bold">Kappaleet levyllä:</div>
+    <div id="albumSongs" style="font-size:18px;" runat="server"></div>
+    <a href="Kuappa.aspx" runat="server">Takaisin</a>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="footer" Runat="Server">
+</asp:Content>
+
---
+++ Saitti/Tehtava6/Albumi.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class Tehtava6_Albumi : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+        string album = Request.QueryString["value"];
+        albumImage.Attributes["src"] = "/Images/" + album + ".jpg";
+
+        XmlDocument xml = new XmlDocument();
+        xml.Load(Server.MapPath("~/App_Data/Kauppa.xml"));
+
+        string info = string.Format("/Records/genre[@name = 'Pop']/record[@ISBN = '{0}']", album);
+        string song = string.Format("/Records/genre[@name = 'Pop']/record[@ISBN = '{0}']/song", album);
+        XmlNodeList xnlist1 = xml.SelectNodes(info);
+        XmlNodeList xnlist2 = xml.SelectNodes(song);
+
+        foreach (XmlNode xn in xnlist1) {
+            albumInfo.InnerHtml += " " + xn.Attributes["Artist"].Value;
+            albumInfo.InnerHtml += " - " + xn.Attributes["Title"].Value + "<br><br> ";
+            albumInfo.InnerHtml += "ISBN: " + xn.Attributes["ISBN"].Value + "<br><br>";
+            albumInfo.InnerHtml += "Hinta: " + xn.Attributes["Price"].Value;
+        }
+
+        foreach (XmlNode xn in xnlist2) {
+            albumSongs.InnerHtml += xn.Attributes["name"].Value + "<br>";
+        }
+    }
+}
