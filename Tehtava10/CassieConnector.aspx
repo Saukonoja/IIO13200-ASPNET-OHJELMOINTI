7c752feb7d121d64311a7e5779767a9a1fdc55d3
Saukonoja <timo.saukonoja@gmail.com>
2016-11-30 03:06:33 +0200

    Add Tehtava10

---
+++ Tehtava10/CassieConnector.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="CassieConnector.aspx.cs" Inherits="CassieConnector" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="float:left; margin-right:100px;">
+            <asp:Button ID="btnGetAll" Text="Get all users" runat="server" OnClick="btnGetAll_Click" />
+            <asp:GridView ID="gvUsers" runat="server"></asp:GridView>
+        </div>
+        <div style="float:left;">
+            Lastname(PRIMARY KEY)<br /><asp:TextBox ID="txtLastname" runat="server"></asp:TextBox><br />
+            Firstname<br /><asp:TextBox ID="txtFirstname" runat="server"></asp:TextBox><br />
+            
+            Age<br /><asp:TextBox ID="txtAge" runat="server"></asp:TextBox>
+            <asp:Button ID="btnInsert" runat="server" Text="Insert into Cassandra" OnClick="btnInsert_Click" />
+            <asp:Button ID="btnUpdate" runat="server" Text="Update user" OnClick="btnUpdate_Click" />
+            <asp:Button ID="btnDelete" runat="server" Text="Delete (give lastname only)" OnClick="btnDelete_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Tehtava10/CassieConnector.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Cassandra;
+using System.Data;
+
+
+public partial class CassieConnector : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+        if (!IsPostBack) {
+
+        }
+        
+    }
+
+    protected ISession connect() {
+        Cluster cluster = Cluster.Builder().AddContactPoint("84.251.189.202").WithPort(9042).Build();
+        ISession session = cluster.Connect("test");
+        return session;
+    }
+
+    protected void insert(ISession session, string firstname, string lastname, int age) {
+        session.Execute("insert into users (firstname, lastname, age) values ('" + firstname + "','" + lastname +"'," + age + " );");
+    }
+
+    protected void update(ISession session, string firstname, string lastname, int age) {
+        session.Execute("update users set firstname= '" + firstname + "' , lastname='" + lastname + "', age='" + age + "' where lastname='"+lastname+"';");
+    }
+
+    protected Row select(ISession session, string lastname) {
+        Row result = session.Execute("select * from users where lastname='" + lastname + "';").First();
+        return result;
+    }
+
+    protected void delete(ISession session, string lastname) {
+        session.Execute("delete from users where lastname ='" + lastname + "';");
+    }
+
+    protected void selectAll(ISession session) {
+        var results = session.Execute("select * from users;");
+
+
+    }
+
+    protected void btnInsert_Click(object sender, EventArgs e) {
+        string firstname = txtFirstname.Text;
+        string lastname = txtLastname.Text;
+        int age = int.Parse(txtAge.Text);
+        insert(connect(), firstname, lastname, age);
+    }
+
+    protected void btnGetAll_Click(object sender, EventArgs e) {
+        ISession session = connect();
+        DataTable dt = new DataTable();
+        var results = session.Execute("select * from users;");
+        foreach (var row in results) {
+            string lastname = row.GetValue<string>(0);
+            int age = row.GetValue<int>(1);
+            string firstname = row.GetValue<string>(2);
+            dt.Rows.Add(lastname, age, firstname);
+        }
+        gvUsers.DataSource = dt;
+        gvUsers.DataBind();
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e) {
+        string lastname = txtLastname.Text;
+        delete(connect(), lastname);
+    }
+
+    protected void btnUpdate_Click(object sender, EventArgs e) {
+        string firstname = txtFirstname.Text;
+        string lastname = txtLastname.Text;
+        int age = int.Parse(txtAge.Text);
+        update(connect(), firstname, lastname, age);
+    }
+}
